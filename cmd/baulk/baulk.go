0fc0de4bbe5eaddcb09f173b480ecc34b4edcf99
Force Charlie <ipvb@qq.com>
2019-09-25 14:52:43 +0800

    Update go mod

--- cmd/baulk/baulk.go
+++ cmd/baulk/baulk.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"os"
+
+	"github.com/baulk/baulk/command"
+)
+
 var (
 	version = "0.0.0"
 	commit  = "dev"
@@ -9,4 +15,10 @@ var (
 // -E command
 func main() {
 	//command.Exec(os.Args)
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+	if os.Args[1] == "-E" {
+		os.Exit(command.Exec(os.Args))
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/baulk/baulk
 
 go 1.13
+
+require golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392
